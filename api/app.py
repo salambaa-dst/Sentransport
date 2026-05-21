6739921984893ec925988e512de09ef3923448b3
Selemba sall <sallselemba@gmail.com>
2026-05-21 20:47:40 +0000

    Lab 5 : React consomme Flask via fetch et useEffect

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,17 +9,20 @@ CORS(app)
 with open("lignes_ddd.json", "r") as f:
     lignes = json.load(f)
 
+# ─── Endpoint accueil 
 @app.route("/")
 def accueil():
     return jsonify({
         "message": "Bienvenue sur l'API SenTransport !",
-        "endpoints": ["/lignes", "/lignes/<id>"]
+        "endpoints": ["/lignes", "/lignes/<id>", "/arrets", "/stats", "/lignes/recherche?q="]
     })
 
+# ─── Endpoint
 @app.route("/lignes")
 def get_lignes():
     return jsonify(lignes)
 
+# ── Endpoint : une ligne par id
 @app.route("/lignes/<int:ligne_id>")
 def get_ligne(ligne_id):
     ligne = next(
@@ -29,6 +32,38 @@ def get_ligne(ligne_id):
     if ligne is None:
         return jsonify({"erreur": "Ligne non trouvee"}), 404
     return jsonify(ligne)
+
+# ───Exercice 1
+@app.route("/arrets")
+def get_arrets():
+    tous_les_arrets = set()
+    for ligne in lignes:
+        for arret in ligne["listeArrets"]:
+            tous_les_arrets.add(arret)
+    return jsonify(list(tous_les_arrets))
+
+# ─ Exercice2
+@app.route("/stats")
+def get_stats():
+    nombre_lignes = len(lignes)
+    total_arrets = sum(l["arrets"] for l in lignes)
+    ligne_max = max(lignes, key=lambda l: l["arrets"])
+    return jsonify({
+        "nombre_lignes": nombre_lignes,
+        "total_arrets": total_arrets,
+        "ligne_plus_darrets": ligne_max["numero"]
+    })
+
+# Exercice 3 
+@app.route("/lignes/recherche")
+def recherche():
+    q = request.args.get("q", "").lower()
+    resultats = [
+        l for l in lignes
+        if q in l["depart"].lower() or q in l["arrivee"].lower()
+    ]
+    return jsonify(resultats)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
